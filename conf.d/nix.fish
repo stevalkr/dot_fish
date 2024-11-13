b1d75e40db47f0e9832642bfafaeed64354bc31a
Steve Walker <65963536+etherswangel@users.noreply.github.com>
2024-11-13 18:26:23 +0800

    fix complete path & add bass

--- conf.d/nix.fish
+++ conf.d/nix.fish
@@ -1,5 +1,5 @@
 if test -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
     . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish'
-    set -x fish_complete_path $fish_complete_path $HOME/.nix-profile/share/fish/vendor_completions.d
+    set fish_complete_path $fish_complete_path $HOME/.nix-profile/share/fish/vendor_completions.d
 end
 
