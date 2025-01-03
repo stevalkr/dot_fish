2ef52992e75cfde67062e3452496a42f21a75f3d
Steve Walker <65963536+etherswangel@users.noreply.github.com>
2025-01-03 20:57:19 +0800

    remove s, fix python completions

--- functions/s.fish
+++
@@ -1,4 +0,0 @@
-function s --wraps='ssh -Y -L 8888:localhost:8888' --wraps='kitty +kitten ssh -Y -L 8888:localhost:8888' --description 'alias s kitty +kitten ssh -Y -L 8888:localhost:8888'
-  kitty +kitten ssh -Y -L 8888:localhost:8888 $argv
-        
-end
